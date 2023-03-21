9d25ac6de47ac75b8e3542371bc0a726d15da467
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-03-21 16:34:11 +0100

    [add] funzionamento di base

--- assets/js/scrypt.js
+++ assets/js/scrypt.js
@@ -1,4 +1,7 @@
 const priceKm = 0.21;
+const seniorDiscount = 40;
+const juniorDiscount = 20;
+const output = document.getElementById('output');
 const getTicket = document.getElementById('getTicket');
 
 getTicket.addEventListener('click', function(){
@@ -7,6 +10,25 @@ getTicket.addEventListener('click', function(){
   let age = document.getElementById('age').value;
   console.log(username, distance, age);
 
-  const totalPrice = distance * priceKm;
-  console.log(totalPrice);
+  const normalPrice = distance * priceKm;
+  let discountPrice;
+  let message;
+  if(age === 'junior'){
+    discountPrice = normalPrice * (1 - (juniorDiscount/100));
+    message = `
+    Ciao ${username}, il prezzo del tuo biglietto è ${discountPrice.toFixed(2)}\u20AC invece di ${normalPrice}
+    `
+    output.innerHTML = message
+  }else if (age === 'senior'){
+    discountPrice = normalPrice * (1 - (seniorDiscount/100));
+    message = `
+    Ciao ${username}, il prezzo del tuo biglietto è ${discountPrice.toFixed(2)}\u20AC invece di ${normalPrice}
+    `
+    output.innerHTML = message
+  }else{
+    message = `
+    Ciao ${username}, il prezzo del tuo biglietto è ${normalPrice}\u20AC 
+    `
+    output.innerHTML = message
+  }
 })
